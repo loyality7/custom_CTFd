b1d2a8ef4eb8feba420c1ff1232711077e18af39
loyality7 <sarathyadav112@gmail.com>
2024-05-14 11:47:23 +0530

    Squashed 'ctf-challenges/' content from commit 33ef02b
    git-subtree-dir: ctf-challenges
    git-subtree-split: 33ef02b4ecec332c6e7d5f5511a930747645ba8f

---
+++ linux/HTB 0x2/routes/zip.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const unzipper = require('unzipper');
+
+var upload = multer({ dest: '/home/administrator/uploads/zips' })
+const router = express.Router();
+
+router.post('/extract', upload.single('zipFile'), (req, res, next) => {
+    console.log(req.file);
+    fs.createReadStream(req.file.path).pipe(unzipper.Extract({ path: '/home/administrator/uploads/extracts' }))
+        .on('error', (e) => {
+            res.json({
+                success: false,
+                error: e,
+            });
+        }).on('finish', () => {
+            res.json({
+                success: true
+            });
+        })
+});
+
+module.exports = router;
---
+++ linux/HTB 0x2/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const hbs = require('express-handlebars');
+const path = require('path');
+const User = require('./models/user');
+const adminRouter = require('./routes/admin');
+const zipRouter = require('./routes/zip');
+
+require('./models/db');
+
+const app = express();
+
+const publicFolder = path.join(__dirname, 'public');
+app.set('views', publicFolder);
+app.engine(
+    'html',
+    hbs({
+        extname: '.html',
+        defaultLayout: path.join(publicFolder, 'layouts', 'main'),
+        layoutsDir: path.join(publicFolder, 'layouts'),
+    }),
+);
+
+app.set('view engine', 'html');
+
+app.use(session({
+    secret: 'b1gb24int1m3',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/robots.txt', (req, res) => {
+    res.send('Disallow: /admin');
+});
+
+app.post('/', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username, password });
+
+        if (!user) {
+            res.send(`No user with username: ${username} and password: ${password}.`);
+            return;
+        }
+
+        req.session.user = username;
+        res.redirect('/home');
+    } catch {
+        res.send('An unexpected error occured.');
+    }
+});
+
+function isLoggedIn(req, res, next) {
+    if (!req.session.user) {
+        res.redirect('/');
+        return;
+    }
+    next();
+}
+
+app.get('/home', isLoggedIn, (req, res) => {
+    res.render('home');
+});
+
+app.use('/admin', adminRouter);
+app.use('/zip', zipRouter);
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
